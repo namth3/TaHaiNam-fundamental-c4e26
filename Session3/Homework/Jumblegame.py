dac08a36f747311388346cf7b59570c9ae32d7fa
namth3 <daihiep1707@gmail.com>
2019-02-11 01:25:38 +0700

    second commit

---
+++ Session3/Homework/Jumblegame.py
@@ -0,0 +1,17 @@
+import random
+Dictionary = ["bicycle","football","dog","doctor"]
+jumble = True
+words = random.choice(Dictionary)
+n = len(words)
+num_list = list(range(0,n))
+random.shuffle(num_list)
+while jumble == True: 
+    for i in range(0,n):
+        print(words[num_list[i]],end=" ")
+    print()
+    Your_guess = input("Guess the word: ")
+    if Your_guess == words:
+        print("Bingo")
+        jumble = False
+    else:
+        print("Incorrect. Try again")
